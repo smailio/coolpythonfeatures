c497b0d307090c07a9ed4adfd2ea767fe165ee2f
Anis <smail.a.anis@gmail.com>
2020-02-18 11:46:49 +0100

    update structure

--- functions/functions.py
+++ functions/functions.py
@@ -1,28 +1,10 @@
 def make_wrap(*args):
+    pass
 
-    def wrap(sWord):
-        sRes = sWord
-        l = list(args)
-        if len(l) == 1:
-            sRes = l[0] + sRes + l[0]
-        else:
-            sRes = l[0] + sRes + l[1]
-        return sRes
-    return wrap
 
 def make_append_only_list():
+    pass
 
-    l1 = []
-    def funcAppend(dictArg):
-        l1.append(dictArg)
-
-    def funcGet()  :
-        return l1.copy()
-
-    return funcAppend,funcGet
 
 def mymap(listArg, funcArg):
-    listRes = []
-    for lArg in listArg:
-        listRes.append(funcArg(lArg))
-    return listRes
+    pass
